b225c02d84268c9ac67273575718eea455405821
Riya Dev <riyasdev9@gmail.com>
2023-12-27 11:32:37 -0500

    TMDB Discover Movies: keywords works.

--- main.py
+++ main.py
@@ -21,7 +21,26 @@ def get_genre_ids(api_key, genre_names):
     genre_ids = [genres_dict.get(name.lower(), None) for name in genre_names]
 
     return [str(genre_id) for genre_id in genre_ids if genre_id is not None]
- 
+
+def get_keyword_ids(api_key, keywords):
+    base_url = 'https://api.themoviedb.org/3/search/keyword'
+    keyword_ids = []
+
+    for keyword in keywords.split():
+        params = {'api_key': api_key, 'query': keyword}
+        response = requests.get(base_url, params=params)
+
+        if response.status_code == 200:
+            results = response.json().get('results', [])
+            if results:
+                # Assuming you want the ID of the first keyword in the list
+                keyword_ids.append(str(results[0]['id']))
+            else:
+                print(f"No keyword found for '{keyword}'.")
+        else:
+            print(f"Error: {response.status_code}")
+
+    return '|'.join(keyword_ids)
 
 def discover_movies(api_key, include_adult, primary_release_date_gte, primary_release_date_lte, vote_average_gte, runtime_gte, runtime_lte, genres, keywords):
     # https://developer.themoviedb.org/reference/discover-movie
@@ -29,7 +48,6 @@ def discover_movies(api_key, include_adult, primary_release_date_gte, primary_re
     
     params = {
         "api_key": api_key,
-        "limit": 10,
         "include_adult": include_adult,
         # "language": "en",
         "primary_release_date.gte": primary_release_date_gte,
@@ -53,6 +71,7 @@ def discover_movies(api_key, include_adult, primary_release_date_gte, primary_re
 
         for movie in movies:
             print(f"Title: {movie.get('title')}, Release Date: {movie.get('release_date')}, Vote Average: {movie.get('vote_average')}, Language: {movie.get('original_language')}")
+            # title, release_date, vote_average, original_language, overview, genre_ids
 
         return movies
     else:
@@ -75,17 +94,19 @@ response = requests.get(url, headers=headers)
 # print("with_genres Parameter:", "|".join(genre_ids))
 
 include_adult = False
-primary_release_date_gte = "2018-01-01"
+primary_release_date_gte = "2021-01-01"
 primary_release_date_lte = "2021-12-31"
 vote_average_gte = 7.0
-runtime_gte = 30
-runtime_lte = 180
+runtime_gte = 60
+runtime_lte = 150
 
-genre_strings = ["Comedy", "Romance"]
+genre_strings = [] #"Romance", "Comedy"
 genre_ids = get_genre_ids(api_key, genre_strings)
 genres = "|".join(genre_ids)
 
-keywords = ""
+keyword_words = ""
+keywords = get_keyword_ids(api_key, keyword_words)
+# print(keywords)
 
 movies = discover_movies(api_key, include_adult, primary_release_date_gte, primary_release_date_lte, vote_average_gte, runtime_gte, runtime_lte, genres, keywords)
-print(movies)
+print("\n", movies)
