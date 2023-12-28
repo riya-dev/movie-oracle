06c847634bd4239e232deee8fd6d347169650ded
Riya Dev <riyasdev9@gmail.com>
2023-12-28 01:18:29 -0500

    Successfully linked frontend to retrieve data from backend api

---
+++ backend/server.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+#app instance
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/api/home", methods=['GET'])
+def return_home():
+    return jsonify({
+        'message': "Testing link to client side from server side!",
+        'people': ['Riya', 'Nikki']
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)
--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -1,13 +1,46 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+// import Head from 'next/head'
+// import Image from 'next/image'
+// import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+// export default function Home() {
+//   return (
+//     <>
+//       <div>Hello World</div>
+//     </>
+//   )
+// }
+
+import React, { useEffect, useState } from "react";
+
+function index() {
+
+  const[message, setMessage] = useState("Loading");
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    // fetch API
+    fetch("http://localhost:8080/api/home")
+    .then((response) => response.json())
+    .then((data) => {
+      // console.log(data);
+      // message initially set to 'Loading'.
+      // once data is retrived, message is set to data.message
+      setMessage(data.message);
+      // console.log(people);
+      setPeople(data.people);
+    });
+  }, [])
+
   return (
-    <>
-      <div>Hello World</div>
-    </>
+    <div>
+      <div>{message}</div>
+      {people.map((person, index) =>
+        <div key={index}>{person}</div>
+      )}
+    </div>
   )
 }
+
+export default index
